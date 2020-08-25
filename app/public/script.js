2f736c428aa639e5ea7d6e51536bfc91f9fe6d9c
JoaoSouza0 <jooavictorcostasouza15@gmail.com>
2020-08-25 07:28:28 -0300

    Update: Adicionando funções initTab, initScrollSmooth e initShowSection

--- app/public/script.js
+++ app/public/script.js
@@ -1,22 +1,123 @@
-const imgAnimals = document.querySelectorAll('.js-animals-imgList li')
-const descriptAnimals = document.querySelectorAll('.js-animals-descript section')
+function initTab() {
 
-function addActiveAnimals(index){
+    const imgAnimals = document.querySelectorAll('.js-animals-imgList li')
+    const descriptAnimals = document.querySelectorAll('.js-animals-descript section')
 
-    descriptAnimals.forEach((item)=>{
+    if (imgAnimals.length && descriptAnimals.length) {
+        descriptAnimals[0].classList.add('activeAnimals')
 
-        item.classList.remove('activeAnimals')
+        function addActiveAnimals(index) {
 
-    })
+            descriptAnimals.forEach((item) => {
 
-    descriptAnimals[index].classList.add('activeAnimals')
-    console.log(descriptAnimals[index])
+                item.classList.remove('activeAnimals')
+
+            })
+
+            descriptAnimals[index].classList.add('activeAnimals')
+            console.log(descriptAnimals[index])
+        }
+
+        imgAnimals.forEach((item, index) => {
+
+            item.addEventListener('click', () => {
+                addActiveAnimals(index)
+            })
+
+        })
+    }
 }
 
-imgAnimals.forEach((item, index)=>{
+initTab()
 
-    item.addEventListener('click', ()=>{
-            addActiveAnimals(index)
+
+function initAccordion() {
+
+    const dts = document.querySelectorAll('.js-animals-faq dl dt')
+    const activeAccordion = 'activeAccordion'
+
+    if (dts.length) {
+        dts[0].classList.add(activeAccordion)
+        dts[0].nextElementSibling.classList.add(activeAccordion)
+
+        function accordionList() {
+
+            this.classList.toggle(activeAccordion)
+            this.nextElementSibling.classList.toggle(activeAccordion)
+
+        }
+
+        dts.forEach((item) => {
+            item.addEventListener('click', accordionList)
+
+        })
+    }
+
+}
+initAccordion()
+
+
+function initScrollSmooth() {
+
+
+    function scrollToSection(event) {
+
+        event.preventDefault()
+        const href = this.getAttribute('href')
+        const section = document.querySelector(href)
+
+        section.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+
+        /* window.scrollTo({
+        top: section.offsetTop,
+        behavior: 'smooth',
+
+        }) */
+    }
+
+
+    const linksInternal = document.querySelectorAll('.js-menu a[href^="#"]')
+
+    linksInternal.forEach((item) => {
+
+        item.addEventListener('click', scrollToSection)
+
     })
 
-})
+}
+
+initScrollSmooth()
+
+function initShowSection() {
+
+    const allSection = document.querySelectorAll('.js-section')
+
+    if (allSection.length) {
+        const windowHalf = window.innerHeight * 0.3
+
+
+        function showSection() {
+
+            allSection.forEach((item) => {
+
+                const itemTop = item.getBoundingClientRect().top - windowHalf
+                const isSectionVisible = itemTop - windowHalf
+
+                if (isSectionVisible < 0) {
+                    item.classList.add('activeSection')
+                }
+
+            })
+
+        }
+
+        showSection()
+        window.addEventListener('scroll', showSection)
+    }
+
+}
+
+initShowSection()
