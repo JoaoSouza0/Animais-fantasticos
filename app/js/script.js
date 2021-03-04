549302d07730bc4a64f6690c00455347effaf11c
Joe <joaovictorcostasouza15@gmail.com>
2021-03-04 13:25:40 -0300

    Iniciando novos metodos refatorados

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,5 @@
 import ScrollSuave from './modules/scrollSuave.js';
-
-import initModal from './modules/modal.js';
-import initToolTip from './modules/toolTip.js';
+import Modal from './modules/modal.js';
 import initDropDown from './modules/dropDown.js';
 import initMenuMobile from './modules/menuMobile.js';
 import initFuncionamento from './modules/funcionamento.js';
@@ -10,6 +8,7 @@ import initBtcFetch from './modules/bitcoinFetch.js';
 import Accordion from './modules/accordion.js';
 import initScrollAnima from './modules/scrollAnimacao.js';
 import TabNav from './modules/animacaoTab.js';
+import ToolTip from './modules/toolTip.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] li a[href^="#"]');
 scrollSuave.init();
@@ -20,8 +19,11 @@ accordion.init();
 const tab = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tab.init();
 
-initModal();
-initToolTip();
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="close"]', '[data-modal="container"]');
+modal.init();
+
+const toolTip = new ToolTip('[data-mapa]');
+toolTip.init();
 initDropDown();
 initMenuMobile();
 initFuncionamento();
